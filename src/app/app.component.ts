0b50e67ff273dda325de172e14ae8086fc63915c
Alex Colombo <uricaine@hotmail.com>
2021-02-26 11:29:41 +0100

    modularity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  frase: string = "AlEx & MaRta";
+  frase: string = "AlEx & MaRia";
   mostrarNombre(){
     console.log(this.frase)
   }
