073c5494a08767de3425aa1f5255012a7af6e176
Alex Colombo <uricaine@hotmail.com>
2021-02-28 18:05:13 +0100

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
-import { PrimeNgModule} from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
   AppRoutingModule,
   BrowserModule,
-  PrimeNgModule,
   SharedModule
   ],
   providers: [],
